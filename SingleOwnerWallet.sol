1752f3516cc8f836eb47b95dd47712f863b0387c
detla4-seeker <9165prashant@gmail.com>
2023-03-25 16:18:28 +0545

    single owner wallet

---
+++ SingleOwnerWallet.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.4.22 <0.9.0;
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+/// @title A contract for demonstrate Wallet Smart Contract
+/// @author Jitendra Kumar
+/// @notice For now, this contract just show how to depositing money into the Owner's account instead of the contract account.
+contract wallet{
+
+	address payable public owner ; 
+
+	constructor() {
+		owner = payable(msg.sender) ; 
+	}
+
+	modifier onlyOwner(){
+		require(msg.sender == owner , "Not owner of this wallet");
+		_;
+	}
+
+	function withdraw(address payable _to , uint256 _amount ) payable public onlyOwner{
+		_to.transfer(_amount) ; 
+	}
+	function depositeAmount() public payable {
+	}
+
+}
